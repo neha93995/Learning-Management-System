09bcfe6dc29642d3103b3dceb87f5e2d1ce66bbd
neha93995 <nehabaghelcs190302@gmail.com>
2023-10-24 17:06:52 +0530

    making home layout and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
-import {Routes} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import Footer from './Components/Footer';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    // <Routes>
-    //   {/* <Route path='/' element={<Home/>} /> */}
-    // </Routes>
-    <>
-      <Footer/>
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+    </Routes>
+
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui"),require("@tailwindcss/line-clamp")],
 }
 
