840f01c22d77d2e9039d837cd5584a55b7e711d6
neha93995 <nehabaghelcs190302@gmail.com>
2023-10-25 17:51:07 +0530

    Adding Page Not Found Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/about' element={<AboutUs/>} />
+
+
+      <Route path='*' element={<NotFound/>} />
     </Routes>
 
   );
